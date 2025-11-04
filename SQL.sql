721987db5b8205d27e58632e02f9e34a14bfbd80
Madhura414 <madhurareddy1252@gmail.com>
2025-11-04 17:15:33 +0530

    querry view Updated

--- SQL.sql
+++ SQL.sql
@@ -1,1 +1,17 @@
 select * from Employees
+Select *from orders
+
+
+Create VIEW [Employees_latest2] AS
+Select emp_number ,emp_id,emp_name from Employees
+where emp_name='smith';
+
+
+select * from Employees_latest2
+
+Create view [Employee_latest_Data] AS
+Select emp_number,emp_id from employees where emp_name='kind';
+
+Select *from Employee_latest_Data
+
+
